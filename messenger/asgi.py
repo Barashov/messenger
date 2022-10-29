63f5fbd13b88093ed0ff61bda4ec370b5ee00136
Barashov <BarashovMisha@gmail.com>
2022-10-29 14:53:47 +0300

    channels integretion

--- messenger/asgi.py
+++ messenger/asgi.py
@@ -1,16 +1,15 @@
-"""
-ASGI config for messenger project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+from chat.routing import ws_urlpatterns
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messenger.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns))
+})
